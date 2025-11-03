e926d00aa5be5dcb42c4a9174f03d5c1c6db52d5
AshrafGalib <galibashraf.ag@gmail.com>
2025-11-03 14:16:15 +0600

    daisy and tailwind installled

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Nabvar from './assets/components/navbar/Nabvar'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Nabvar></Nabvar>
+    </>
+  )
+}
+
+export default App
